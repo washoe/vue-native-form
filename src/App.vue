0be4e17a171a0d3b16409832456120a42a6823bf
washoe <b.oliver@optusnet.com.au>
2019-04-06 13:02:43 +1100

    Basic form wrapper
    Logs form to console on submit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormWrapper/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormWrapper from './components/FormWrapper.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FormWrapper
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/FormWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My Form</h1>
+    <form @submit.prevent="handleSubmit">
+      <label for="text">Text</label>
+      <input type="text" name="text"/>
+      <button type="submit">submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "FormWrapper",
+  props: {
+    formData: Object
+  },
+  methods: {
+    handleSubmit: function (event) {
+      console.log('form submit', event.target);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
